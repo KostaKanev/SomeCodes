c89bc7ebebdccd0521760e4e15c8a1e9a612aae3
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-23 18:22:26 +0300

    Add new file and rename old

---
+++ Telerik Academy Exam 5 December 2013 Evening/Problem 3 OnesAndZeros.cs
@@ -0,0 +1,54 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        string binary = Convert.ToString(input , 2).PadLeft(32, '0');
+
+        string zeroTopBot = "###";
+        string zeroBody = "#.#";
+        string oneTopBody = ".#.";
+        string oneUnique = "##.";
+        string oneBot = zeroTopBot;
+
+        for (int row = 0; row < 5; row++)
+        {
+            for (int bit = 16; bit < binary.Length; bit++)
+            {
+                if (binary[bit] == '0')
+                {
+                    if (row == 0 || row == 4)
+                    {
+                        Console.Write(zeroTopBot);
+                    }
+                    else
+                    {
+                        Console.Write(zeroBody);
+                    }
+                }
+                else
+                {
+                    if (row == 1)
+                    {
+                        Console.Write(oneUnique);
+                    }
+                    else if (row == 4)
+                    {
+                        Console.Write(oneBot);
+                    }
+                    else
+                    {
+                        Console.Write(oneTopBody);
+                    }
+                }
+                if (bit != binary.Length - 1)
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
