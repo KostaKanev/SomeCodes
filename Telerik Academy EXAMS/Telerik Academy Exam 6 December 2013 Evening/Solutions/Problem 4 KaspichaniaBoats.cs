cdecfcb674a0d8483eda7528c6bdc492ae40bd3b
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-24 23:43:19 +0300

    Update

---
+++ Telerik Academy EXAMS/Telerik Academy Exam 6 December 2013 Evening/Solutions/Problem 4 KaspichaniaBoats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class KaspichaniaBoats
+{
+    public static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        int height = 6 + ((size - 3) / 2) * 3;
+        int width = size * 2 + 1;
+
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                if (row == size || col == size ||
+                    (row == height - 1 && (col >= size / 2 && col < (size + 1) + (size / 2) )))
+                {
+                    Console.Write('*');
+                }
+                else if ( (row + col == size) || 
+                    (row + col == size * 3 ))
+                {
+                    Console.Write('*');
+                }
+                else if ( (row - col == size) || 
+                    (row - col == -size) )
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
