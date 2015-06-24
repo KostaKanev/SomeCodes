fa31f4819c5bf66e55b680cd45d9e17cc5dabdde
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-24 17:34:46 +0300

    Update

---
+++ Telerik Academy EXAMS/Telerik Academy Exam 6 December 2013 Evening/Solutions/Problem 2 NightmareOnCodeStreet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class NightmareOnCodeStreet
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+
+        int sumOfDigits = 0;
+        int countOfDigits = 0;
+
+        for (int i = 1; i < text.Length; i+=2)
+        {
+            sumOfDigits += text[i] - '0';
+            countOfDigits++;
+        }
+        Console.WriteLine("{0} {1}" , countOfDigits , sumOfDigits );
+    }
+}
