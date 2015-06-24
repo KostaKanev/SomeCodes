d36a94d15965af8b7c809c13ddde7d03c591a6df
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-24 16:42:50 +0300

    Update

---
+++ Telerik Academy EXAMS/Telerik Academy Exam 6 December 2013 Evening/Solutions/Problem 1 TwoFourEight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class TwoFourEight
+{
+    public static void Main()
+    {
+        // read input from console
+        int numberA = int.Parse(Console.ReadLine());
+        int numberB = int.Parse(Console.ReadLine());
+        int numberC = int.Parse(Console.ReadLine());
+
+        
+        int result = 0;
+        // check for numberB if == 2 || 4 || 8
+        if (numberB == 2)
+        {
+            result = numberA % numberC;
+        }
+        else if (numberB == 4)
+        {
+            result = numberA + numberC;
+        }
+        else if (numberB == 8)
+        {
+            result = numberA * numberC;
+        }
+
+        // check for result divided by 4
+        if (result % 4 == 0)
+        {
+            Console.WriteLine(result / 4);
+        }
+        else
+        {
+            Console.WriteLine(result % 4);
+        }
+        Console.WriteLine(result);
+    }
+}
+
