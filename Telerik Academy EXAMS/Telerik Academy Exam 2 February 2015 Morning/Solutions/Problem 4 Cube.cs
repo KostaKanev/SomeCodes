ffc8bceaea9f98c95f8d0e2b67e17bad029e9cf4
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-25 12:12:27 +0300

    Update

---
+++ Telerik Academy EXAMS/Telerik Academy Exam 2 February 2015 Morning/Solutions/Problem 4 Cube.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Cube
+{
+    static void Main()
+    {
+        int n = 5; // int.Parse(Console.ReadLine());
+
+        Console.Write(new string(' ', n - 1));
+        Console.Write(new string(':', n));
+        Console.Write(new string(' ', n - 2));
+        Console.WriteLine();
+        Console.Write(new string(' ', n - 2));
+        Console.Write(':');
+        Console.Write(new string('/', n - 2));
+        Console.Write(new string(':', 2));
+        Console.WriteLine();
+
+        for (int i = 0; i < n - 3; i++)
+        {
+            Console.Write(new string(' ', ((n - 3) - i)));
+            Console.Write(':');
+            Console.Write(new string('/', n - 2));
+            Console.Write(':');
+            Console.Write(new string('X', (i + 1)));
+            Console.Write(':');
+            Console.WriteLine();
+        }
+
+        Console.Write(new string(':', n));
+        Console.Write(new string('X', n - 2));
+        Console.Write(':');
+        Console.WriteLine();
+
+        for (int i = 0; i < n - 3; i++)
+        {
+            Console.Write(':');
+            Console.Write(new string(' ', n - 2));
+            Console.Write(':');
+            Console.Write(new string('X', ((n - 3) - i)));
+            Console.Write(':');
+            Console.WriteLine();
+        }
+
+        Console.Write(':');
+        Console.Write(new string(' ', n - 2));
+        Console.Write(new string(':', 2));
+        Console.WriteLine();
+        Console.Write(new string(':', n));
+        Console.WriteLine();
+    }
+}
