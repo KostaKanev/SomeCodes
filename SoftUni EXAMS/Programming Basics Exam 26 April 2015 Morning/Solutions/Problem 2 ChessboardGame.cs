79d3e9a48a574971b5ab2eb07e7925aa87186663
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-25 17:10:44 +0300

    Update

---
+++ SoftUni EXAMS/Programming Basics Exam 26 April 2015 Morning/Solutions/Problem 2 ChessboardGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+public class ChessboardGame
+{
+    public static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        string input = Console.ReadLine();
+        
+        int whiteScore = 0;
+        int blackScore = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i >= size * size)
+            {
+                break;
+            }
+            if (i % 2 == 0 && char.IsUpper(input[i]))
+            {
+                whiteScore += input[i];
+            }
+            else if (i % 2 == 0 && char.IsLetterOrDigit(input[i]))
+            {
+                blackScore += input[i];
+            }
+            else if (i % 2 != 0 && char.IsUpper(input[i]))
+            {
+                blackScore += input[i];
+            }
+            else if (i % 2 != 0 && char.IsLetterOrDigit(input[i]))
+            {
+                whiteScore += input[i];
+            }
+        }
+        
+        if (whiteScore < blackScore)
+        {
+            Console.WriteLine("The winner is: black team");
+            Console.WriteLine(blackScore - whiteScore);
+        }
+        else if (whiteScore == blackScore)
+        {
+            Console.WriteLine("Equal result: " + blackScore);
+        }
+        else
+        {
+            Console.WriteLine("The winner is: white team");
+            Console.WriteLine(whiteScore - blackScore);
+        }
+    }
+}
---
+++ SoftUni EXAMS/Programming Basics Exam 26 April 2015 Morning/Solutions/Problem 3 KingofThieves.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class KingofThieves
+{
+    public static void Main(string[] args)
+    {
+        int size = 13; // int.Parse(Console.ReadLine());
+        string input = "a"; // Console.ReadLine();
+        char gem = input[0];
+        int helper = size / 2;
+        int numOfGems = 1;
+ 
+        for (int i = 1; i <= size / 2; i++)
+        {
+            
+            Console.Write(new string('-', helper));
+            Console.Write(new string(gem , numOfGems));
+            Console.Write(new string('-', helper));
+            Console.WriteLine();
+            numOfGems += 2;
+            helper--;
+        }
+        Console.WriteLine(new string(gem , size));
+        numOfGems = size - 2;
+        for (int i = 1; i <= size / 2; i++)
+        {
+            Console.Write(new string('-', i));
+            Console.Write(new string(gem, numOfGems));
+            Console.Write(new string('-', i));
+            Console.WriteLine();
+            numOfGems -= 2;
+        }
+    }
+}
