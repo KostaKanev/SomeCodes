ea829104cf9a3a329d8481314a8e17c624c0f91f
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-26 17:11:11 +0300

    Update

---
+++ SoftUni EXAMS/C# Basics Exam 20 December 2014/Solutions/Problem 3 Boat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class Boat
+{
+    public static void Main()
+    {
+        
+        int size = int.Parse(Console.ReadLine());
+        int leftDots = size - 1;
+
+        for (int i = 1; i <= size; i += 2)
+        {
+            Console.Write("{0}{1}{2}", new string('.', leftDots), new string('*', i), new string('.', size));
+            Console.WriteLine();
+            leftDots -= 2;
+        }
+        leftDots = 2;
+        for (int i = size - 2; i >= 1; i -= 2)
+        {
+            Console.Write("{0}{1}{2}", new string('.', leftDots), new string('*', i), new string('.', size));
+            Console.WriteLine();
+            leftDots += 2;
+        }
+        Console.Write(new string('*', size * 2));
+        Console.WriteLine();
+
+        for (int i = 1; i < (size - 1) / 2; i++)
+        {
+            Console.Write("{0}{1}{0}", new string('.', i), new string('*', (size * 2) - (i * 2)));
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+}
