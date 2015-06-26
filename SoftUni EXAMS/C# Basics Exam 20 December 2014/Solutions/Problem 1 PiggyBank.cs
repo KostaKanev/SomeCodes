82697ff56b14e7e7672d4f6bae545e9c0cdba782
Kosta.Kanev <kosta.kanev@gmail.com>
2015-06-26 14:59:00 +0300

    Update

---
+++ SoftUni EXAMS/C# Basics Exam 20 December 2014/Solutions/Problem 1 PiggyBank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class PiggyBank
+{
+    public static void Main()
+    {
+        int prizeOfTank = int.Parse(Console.ReadLine());
+        int partyDays = int.Parse(Console.ReadLine());
+
+        int cashForMonth = ((30 - partyDays) * 2) - (partyDays * 5);
+        int spendedMoney = partyDays * 5;
+
+        double allMonths = prizeOfTank / (double)cashForMonth;
+
+        double years = Math.Floor(allMonths / 12d);
+        double months = Math.Ceiling(allMonths % 12d);
+        if (partyDays < 8)
+        {
+            Console.WriteLine("never");
+        }
+        else
+        {
+            Console.WriteLine("{0} years, {1} months" , years , months);
+        }
+    }
+}
---
+++ SoftUni EXAMS/C# Basics Exam 20 December 2014/Solutions/Problem 2 CheatSheet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class CheatSheet
+{
+    public static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
+        int startRow = int.Parse(Console.ReadLine());
+        int startCol = int.Parse(Console.ReadLine());
+
+        for (int row = startRow; row < startRow + rows; row++)
+        {
+            for (int col = startCol; col < startCol + cols; col++)
+            {
+                Console.Write(row * col);
+                if (col < startCol + cols - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
