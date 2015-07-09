958e96033a538defd3cbce9b4e89370e9052d6a5
Kosta.Kanev <kosta.kanev@gmail.com>
2015-07-09 23:45:45 +0300

    Update

---
+++ SoftUni EXAMS/CSharp Basics November 2014 Lab/01.Account Information/AccountInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class AccountInformation
+{
+    public static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        int clientID = int.Parse(Console.ReadLine());
+        double totalBalance = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Hello, {0} {1}", firstName, lastName);
+        Console.WriteLine("Client id: {0}", clientID);
+        Console.WriteLine("Total balance: {0:F2}",totalBalance);
+
+        if (totalBalance < 0)
+        {
+            Console.WriteLine("Active: no");
+        }
+        else
+        {
+            Console.WriteLine("Active: yes");
+        }
+    }
+}
---
+++ SoftUni EXAMS/CSharp Basics November 2014 Lab/02.Nums/Nums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Nums
+{
+    public static void Main()
+    {
+        int startNum = int.Parse(Console.ReadLine());
+        int endNum = int.Parse(Console.ReadLine());
+
+        for (int i = startNum; i <= endNum ; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("{0:F3}", Math.Sqrt(i));
+            }
+            else
+            {
+                Console.WriteLine("{0:F3}", Math.Pow(i , 2));
+            }
+        }
+    }
+}
