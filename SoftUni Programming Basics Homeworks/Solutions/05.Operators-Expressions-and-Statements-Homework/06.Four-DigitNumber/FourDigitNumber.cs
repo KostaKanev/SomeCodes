e6fec6dfc20d8bd2b9289916b9d055b3efc044fa
Kosta Kanev <kosta.kanev@gmail.com>
2015-07-24 19:22:38 +0300

    Update

--- SoftUni Programming Basics Homeworks/Solutions/05.Operators-Expressions-and-Statements-Homework/06.Four-DigitNumber/FourDigitNumber.cs
+++ SoftUni Programming Basics Homeworks/Solutions/05.Operators-Expressions-and-Statements-Homework/06.Four-DigitNumber/FourDigitNumber.cs
@@ -18,8 +18,8 @@ class FourDigitNumber
 
         //Print results
         Console.WriteLine("The sum of all digits = {0}", sum);
-        Console.WriteLine("Reversed number = {0}{1}{2}{3}",digits[3],digits[2],digits[1],digits[0]);
-        Console.WriteLine("Last digit in the first = {0}{1}{2}{3}",digits[3],digits[0],digits[1],digits[2]);
-        Console.WriteLine("Exchanges digit 2 and 3 = {0}{1}{2}{3}",digits[0],digits[2],digits[1],digits[3]);
+        Console.WriteLine("Reversed number = {0}{1}{2}{3}", digits[3], digits[2], digits[1], digits[0]);
+        Console.WriteLine("Last digit in the first = {0}{1}{2}{3}", digits[3], digits[0], digits[1], digits[2]);
+        Console.WriteLine("Exchanges digit 2 and 3 = {0}{1}{2}{3}", digits[0], digits[2], digits[1], digits[3]);
     }
 }
