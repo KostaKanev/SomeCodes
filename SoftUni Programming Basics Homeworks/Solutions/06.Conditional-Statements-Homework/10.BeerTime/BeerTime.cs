18cdc3d0d4caec0d39a15a48e5d13efa49f4d908
Kosta Kanev <kosta.kanev@gmail.com>
2015-07-30 22:00:52 +0300

    Update

---
+++ SoftUni Programming Basics Homeworks/Solutions/06.Conditional-Statements-Homework/10.BeerTime/BeerTime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+public class BeerTime
+{
+    public static void Main()
+    {
+
+        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+        
+        try
+        {
+            Console.Write("Please enter a time: ");
+            string input = Console.ReadLine();
+            DateTime time = DateTime.Parse(input);
+            time.ToString("hh:mm tt");
+            DateTime startTime = DateTime.Parse("1:00 PM");
+            DateTime endTime = DateTime.Parse("3:00 AM");
+
+            if (time >= startTime && time <= endTime)
+            {
+                Console.WriteLine("Beer Time");
+            }
+            else
+            {
+                Console.WriteLine("Non Beer Time");
+            }    
+            
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Time");
+        }
+    }
+}
---
+++ SoftUni Programming Basics Homeworks/Solutions/06.Conditional-Statements-Homework/11.NumberAsWords/NumberAsWords.cs
@@ -0,0 +1,61 @@
+﻿using System;
+public class NumberAsWords
+{
+    public static void Main(string[] args)
+    {
+
+        int number = int.Parse(Console.ReadLine());
+        
+        string[] numbersTo19 = new string[]
+        {
+            "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+            "Eight", "Nine", "Ten", "Eleven", "Twelve" ,"Thirteen" , "Fourteen" ,
+            "Fifteen" , "SixTeen","Seventeen", "Eighteen","Nineteen"
+        };
+
+        string[] tenths = new string[]
+        {
+            "Ten", "Twenty","Thirty","Fourty","Fifty","Sixty","Seventy","Eighty","Ninety"
+        };
+        
+        if (number == 0)
+        {
+            Console.WriteLine("Zero");
+            return;
+        }
+        else if (number > 999)
+        {
+            Console.WriteLine("Bigger than 999");
+            return;
+        }
+
+        if (number % 10 == 0 && number < 100)
+        {
+            Console.WriteLine(tenths[(number / 10) - 1]);
+        }
+        else if (number < 20)
+        {
+            Console.WriteLine(numbersTo19[number - 1]);
+        }
+        else if (number < 100)
+        {
+            Console.WriteLine(tenths[(number / 10) - 1] + " " + numbersTo19[(number % 10) - 1]);
+        }
+        else if (number % 100 == 0)
+        {
+            Console.WriteLine(numbersTo19[(number / 100) - 1] + " hundred");
+        }
+        else if (number % 10 == 0)
+        {
+            Console.WriteLine(numbersTo19[(number / 100) - 1] + " hundred and " + tenths[((number / 10) % 10) - 1]);
+        }
+        else if ((number / 10) % 10 < 2)
+        {
+            Console.WriteLine(numbersTo19[(number / 100) - 1] + " hundred and " + numbersTo19[(number % 100) - 1]);
+        }
+        else
+        { 
+             Console.WriteLine(numbersTo19[(number / 100) - 1] + " hundred and " + tenths[((number / 10) % 10) - 1] + " " + numbersTo19[(number % 10) - 1]);
+        }
+    }
+}
