17a3bc46f86d05c0943d96438efbfa72614b2163
Kosta Kanev <kosta.kanev@gmail.com>
2015-08-16 17:38:43 +0300

    Update

---
+++ SoftUni EXAMS/CSharp Basics August 26 2014/Solutions/04.WinningNumbers/WinningNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+public class WinningNumbers
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine().ToLower();
+        int[] digits = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int letSum = 0;
+        foreach (var letter in input)
+        {
+            letSum += letter - '`';
+        }
+
+        int winningNumsCount = 0;
+        foreach (var dig1 in digits)
+        {
+            foreach (var dig2 in digits)
+            {
+                foreach (var dig3 in digits)
+                {
+                    if (dig1 * dig2 * dig3 == letSum)
+                    {
+                        foreach (var dig4 in digits)
+                        {
+                            foreach (var dig5 in digits)
+                            {
+                                foreach (var dig6 in digits)
+                                {
+                                    if (dig4 * dig5 * dig6 == letSum)
+                                    {
+                                        Console.WriteLine("{0}{1}{2}-{3}{4}{5}",
+                                            dig1,dig2,dig3,dig4,dig5,dig6);
+                                        winningNumsCount++; 
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (winningNumsCount == 0)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
---
+++ SoftUni EXAMS/CSharp Basics August 26 2014/Solutions/05.XBits/XBits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class XBits
+{
+    public static void Main()
+    {
+        string[] binaryNumbers = new string[8];
+        for (int i = 0; i < binaryNumbers.Length; i++)
+        {
+            binaryNumbers[i] = Convert.ToString(int.Parse(Console.ReadLine()), 2).PadLeft(32, '0');
+        }
+        int xBitsCount = 0;
+        for (int row = 0; row < binaryNumbers.Length - 2; row++)
+        {
+            for (int col = 0; col < 32 - 2; col++)
+            {
+                if ((binaryNumbers[row][col] == '1' && binaryNumbers[row][col + 1] == '0' && binaryNumbers[row][col + 2] == '1') &&
+                    (binaryNumbers[row + 1][col] == '0' && binaryNumbers[row + 1][col + 1] == '1' && binaryNumbers[row + 1][col + 2] == '0') && 
+                    (binaryNumbers[row + 2][col] == '1' && binaryNumbers[row + 2][col + 1] == '0' && binaryNumbers[row + 2][col + 2] == '1'))
+                {
+                    xBitsCount++;
+                }
+            }
+        }
+        Console.WriteLine(xBitsCount);
+    }
+}
