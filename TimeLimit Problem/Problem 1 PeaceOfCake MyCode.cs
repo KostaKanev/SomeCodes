269bd36265ece7e5f440a05749a1d445c1e5460f
Kosta Kanev <kosta.kanev@gmail.com>
2015-08-23 10:50:47 -0700

    Update

---
+++ TimeLimit Problem/Problem 1 PeaceOfCake MyCode.cs
@@ -0,0 +1,28 @@
+namespace Problem_1___Peace_of_Cake
+{
+    using System;
+    public class PeaceOfCake
+    {
+        public static void Main()
+        {
+            decimal numberA = decimal.Parse(Console.ReadLine());
+            decimal numberB = decimal.Parse(Console.ReadLine());
+            decimal numberC = decimal.Parse(Console.ReadLine());
+            decimal numberD = decimal.Parse(Console.ReadLine());
+            
+            decimal denominator = numberB * numberD;
+            decimal numerator = (numberA * (denominator / numberB)) + (numberC * (denominator / numberD));
+
+            if (numerator >= denominator)
+            {
+                Console.WriteLine("{0:F0}" , numerator/denominator);
+            }
+            else
+            {
+                Console.WriteLine("{0:F22}" , numerator/denominator);
+            }
+
+            Console.WriteLine("{0}/{1}", numerator , denominator);
+        }
+    }
+}
