eb955504de542077e7064d22b78215a908b5a740
Kosta Kanev <kosta.kanev@gmail.com>
2015-09-04 21:24:11 +0300

    Update

---
+++ SoftUni Fundamentals Homeworks/01.Advanced-CSharp-Arrays-Lists-Stacks-Homework/02.SortArrayUsingSelectionSort/SortArrayUsingSelectionSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class SortArrayUsingSelectionSort
+{
+    public static void Main()
+    {
+        //Read numbers with spaces like string
+        string[] input = Console.ReadLine().Split(' ');
+        int[] numbers = new int[input.Length];
+
+        //Transfer parsed to integer numbers in int array
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(input[i]);
+        }
+        
+        int counts = 0;
+        int helper = 0;
+
+        do
+        {
+            counts = 0;
+            
+            for (int index = 0; index < numbers.Length - 1; index++)
+            {
+                if (numbers[index] > numbers[index + 1])
+                {
+                    helper = numbers[index];
+                    numbers[index] = numbers[index + 1];
+                    numbers[index + 1] = helper;
+
+                    counts++;
+                }
+            }
+        } while (counts != 0);
+
+        foreach (var item in numbers)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
