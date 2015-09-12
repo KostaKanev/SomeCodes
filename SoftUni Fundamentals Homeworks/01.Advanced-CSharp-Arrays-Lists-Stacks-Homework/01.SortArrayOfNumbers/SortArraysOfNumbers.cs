50e993fe58d2c9460fd773bea20a69eb45725b15
Kosta Kanev <kosta.kanev@gmail.com>
2015-09-12 22:32:31 +0300

    Update

---
+++ SoftUni Fundamentals Homeworks/01.Advanced-CSharp-Arrays-Lists-Stacks-Homework/01.SortArrayOfNumbers/SortArraysOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class SortArraysOfNumbers
+{
+    public static void Main()
+    {
+        //Read numbers with spaces like string
+        string[] input = Console.ReadLine().Split(' ');
+        int[] numbers = new int[input.Length];
+
+        //Transfer parsed to integer numbers in int array
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(input[i]);
+        }
+        //Sort the array with function
+        Array.Sort(numbers);
+
+        //Print sorted result
+        foreach (var item in numbers)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
